839097f67e376cd20523878944027847d6229bc3
Jennifer Chhie <jschhie@ucdavis.edu>
2020-08-25 19:53:30 -0700

    added sample file and added ReturnToMainButton. Assumes WHOLE numbers as values for now. TODO: parse by decimal character. Current Balance is updated whenever ReturnToMainButton is Clicked (fixme)

--- expenseTracker.py
+++ expenseTracker.py
@@ -2,65 +2,108 @@
 # path: vim ~/Desktop/projects/py_study/tk_samples/expenseTracker.py
 from tkinter import *
 
+
+def raise_frame(frame):
+    frame.tkraise()
+
+
 class ExpenseTracker:
-	
-	def __init__(self, master):
-		self.master = master
-		
-		self.prompt_frame = Frame(master)
-		self.prompt_frame.pack()
+    
+    def __init__(self, master):
+        self.master = master
+        
+        self.curr_balance = "0"
 
-		self.curr_balance_frame = Frame(master)
-		self.curr_balance_frame.pack()
-		self.curr_balance = "0.00"
+        self.main_frame = Frame(master)
+        self.new_txn_frame = Frame(master)
+        self.history_frame = Frame(master)
+        
+        for f in (self.main_frame, self.new_txn_frame, self.history_frame):
+            f.grid(row=0, column=0, sticky="news")
 
-		self.bottom_frame = Frame(master)
-		self.bottom_frame.pack()
-		
-		self.show_main_menu()
-
-
-	def show_main_menu(self):
-		# Show Prompt
-		prompt = Label(self.prompt_frame, text="Select an Action Below")
-		prompt.grid(columnspan=2)
-
-		# Show Current Balance
-		curr_balance_lab = Label(self.curr_balance_frame, text="Current Balance: ")
-		curr_balance_lab.grid(sticky=E)
-		
-		curr_balance_text = Entry(self.curr_balance_frame)
-		curr_balance_text.insert(END, '$' + self.curr_balance)
-		curr_balance_text.config(state="disabled")
-		curr_balance_text.grid(row=0, column=1)
-
-		# Show Available Actions
-		action_lab = Label(self.bottom_frame, text="Choose Action: ")
-		action_lab.grid(row=1, sticky=E)
-
-		# Show Buttons
-		## New Transaction
-		new_txn_button = Button(self.bottom_frame, text="New Transaction")
-		new_txn_button.bind("<Button-1>", self.make_new_txn)
-		new_txn_button.grid(row=0, column=1, sticky=W)
-		## View History
-		view_hist_button = Button(self.bottom_frame, text="View History")
-		view_hist_button.bind("<Button-1>", self.view_history)
-		view_hist_button.grid(row=1, column=1, sticky=W)
-
-		quit_button = Button(self.bottom_frame, text="Quit", command=self.master.quit)
-		quit_button.grid(row=2, column=1, sticky=W)
+        raise_frame(self.main_frame)
+        self.create_main_frame()
 
 
-	def make_new_txn(self, event):
-		print('Create New Transaction')
-		pass
+    def create_main_frame(self):
+        # Show main prompt
+        Label(self.main_frame, text="Main Menu").grid(row=0, columnspan=2)
+
+        # Show Current Balance
+        Label(self.main_frame, text="Current Balance: ").grid(sticky=E)
+        
+        self.curr_balance_text = Entry(self.main_frame)
+        self.curr_balance_text.insert(END, '$' + self.curr_balance)
+        self.curr_balance_text.config(state="disabled")
+        self.curr_balance_text.grid(row=1, column=1)
+
+        # Show Available Actions
+        Label(self.main_frame, text="Choose an Action: ").grid(row=2, sticky=E)
+
+        # New Transaction Button
+        new_txn_button = Button(self.main_frame, text="New Transaction")
+        new_txn_button.bind("<Button-1>", self.make_new_txn)
+        new_txn_button.grid(row=2, column=1)
+
+        # View History Button
+        view_hist_button = Button(self.main_frame, text="View History")
+        view_hist_button.bind("<Button-1>", self.view_history)
+        view_hist_button.grid(row=3, column=1)
+        
+        # Quit Button
+        quit_button = Button(self.main_frame, text="Quit", command=self.master.quit)
+        quit_button.grid(row=4, column=1)
 
 
-	def view_history(self, event):
-		print('View History')
-		pass
+    def make_new_txn(self, event):
+        # Show new frame and prompt
+        raise_frame(self.new_txn_frame)
+        Label(self.new_txn_frame, text="Create New Transaction").grid(row=0, columnspan=2)
 
+        # Amount Label and Entry
+        Label(self.new_txn_frame, text="Amount: ").grid(row=1, sticky=E)
+        self.user_amount = Entry(self.new_txn_frame)
+        self.user_amount.grid(row=1, column=1)
+
+        # Date Label and Entry
+        Label(self.new_txn_frame, text="Date (MM/DD/YYYY): ").grid(row=2, sticky=E)
+        self.user_date = Entry(self.new_txn_frame)
+        self.user_date.grid(row=2, column=1)
+
+        # Determine if Adding or Deducting from Current Balance
+        deposit_button = Button(self.new_txn_frame, text="Deposit")
+        deposit_button.bind("<Button-1>", self.deposit_money)
+        deposit_button.grid(row=3, column=1)
+
+        # Back to Main Menu Button
+        back_button = Button(self.new_txn_frame, text="Return to Main Menu")
+        back_button.bind("<Button-1>", self.return_to_main)
+        back_button.grid(row=5, column=1)
+
+        pass
+
+    def deposit_money(self, event):
+        # TODO Split curr_balance by '.' decimal delimiter
+        # TODO assumes WHOLE numbers for now
+        self.curr_balance = str(int(self.curr_balance) + int(self.user_amount.get()))
+        print('Current new balance: $', self.curr_balance)
+        pass
+
+
+    def return_to_main(self, event):
+        # TODO Get updated current balance
+        self.curr_balance_text.config(state="normal")
+        self.curr_balance_text.delete(0, END)
+        self.curr_balance_text.insert(END, '$' + self.curr_balance)
+        self.curr_balance_text.config(state="disabled")
+        raise_frame(self.main_frame)
+        pass
+
+
+    def view_history(self, event):
+        print('View History')
+        raise_frame(self.history_frame)
+        pass
 
 
 root = Tk()
