7130dd4d29842c33e330320153c2812a47a853f8
Jennifer Chhie <jschhie@ucdavis.edu>
2023-03-29 12:49:44 -0700

    +init_frames code into new file

--- helper_lib.py
+++ helper_lib.py
@@ -4,6 +4,8 @@ import numpy as np
 
 from tkinter import messagebox
 
+
+
 digit_month_map = {1: "Jan.", 2: "Feb.", 3: "Mar.",
                     4: "Apr.", 5: "May", 6: "Jun.",
                     7: "Jul.", 8: "Aug.", 9: "Sep.",
@@ -32,57 +34,6 @@ def check_view_filters(in_year, in_month=None, ignore_month=False):
             "Please provide valid year%s." % add_str)
         return () # Failure
 
-
-def show_plot(exp_values, exp_labels, u_month, u_year):
-    ''' Creates and displays Pie Chart.'''
-    # Set up pie chart
-    fig, ax = plt.subplots()
-    ax.pie(exp_values, labels=exp_labels, autopct='%1.1f%%', pctdistance=0.85)
-    ax.axis('equal')
-
-    # Draw Donut within Pie
-    center_circle = plt.Circle((0,0), 0.70, fc='white')
-    fig = plt.gcf()
-    fig.gca().add_artist(center_circle)
-
-    # Plot data
-    plt.title('Expenses Report for %s %d' % (digit_month_map[u_month], u_year))
-    plt.tight_layout()
-    plt.show()
-
-
-def autolabel(rects):
-    ''' Sub-helper function to autolabel each bar in the multi-bar graph. ''' 
-    for rect in rects:
-        height = rect.get_height()
-        plt.text(rect.get_x() + rect.get_width() / 2., 1.0 * height,
-        '%d' % int(height),
-        ha='center', va='bottom')
-
-
-def show_bar_chart(total_deposits, total_withdrawals, user_year):
-    ''' Creates and displays the Grouped Bar Chart. '''
-    bar_width = 0.25
-    r1 = np.arange(len(total_deposits))
-    r2 = [x + bar_width for x in r1]
-
-    rects1 = plt.bar(r1, total_deposits, width=bar_width, 
-        edgecolor='white', label='Total Deposits')
-    
-    rects2 = plt.bar(r2, total_withdrawals, width=bar_width, 
-        edgecolor='white', label='Total Withdrawals')
-
-    plt.xlabel('Month')
-    plt.ylabel('Transactions Amount ($)')
-    plt.title('Expenses Report for %d: Transactions Grouped by Month' % user_year)
-    plt.xticks([r + bar_width for r in range(len(total_deposits))], 
-        list((digit_month_map).values()))
-
-    autolabel(rects1)
-    autolabel(rects2)
-
-    plt.legend()
-    plt.show()
 
 
 def check_txn_input(pending_total, pending_change, user_date, is_deposit_txn):
@@ -121,3 +72,58 @@ def check_txn_input(pending_total, pending_change, user_date, is_deposit_txn):
     except:
         messagebox.showerror("Input Error", "Please enter a valid, positive amount.")
     return () # Empty tuple
+
+
+
+def show_plot(exp_values, exp_labels, u_month, u_year):
+    ''' Creates and displays Pie Chart.'''
+    # Set up pie chart
+    fig, ax = plt.subplots()
+    ax.pie(exp_values, labels=exp_labels, autopct='%1.1f%%', pctdistance=0.85)
+    ax.axis('equal')
+
+    # Draw Donut within Pie
+    center_circle = plt.Circle((0,0), 0.70, fc='white')
+    fig = plt.gcf()
+    fig.gca().add_artist(center_circle)
+
+    # Plot data
+    plt.title('Expenses Report for %s %d' % (digit_month_map[u_month], u_year))
+    plt.tight_layout()
+    plt.show()
+
+
+
+def autolabel(rects):
+    ''' Sub-helper function to autolabel each bar in the multi-bar graph. ''' 
+    for rect in rects:
+        height = rect.get_height()
+        plt.text(rect.get_x() + rect.get_width() / 2., 1.0 * height,
+        '%d' % int(height),
+        ha='center', va='bottom')
+
+
+
+def show_bar_chart(total_deposits, total_withdrawals, user_year):
+    # Creates and displays the Grouped Bar Chart.
+    bar_width = 0.25
+    r1 = np.arange(len(total_deposits))
+    r2 = [x + bar_width for x in r1]
+
+    rects1 = plt.bar(r1, total_deposits, width=bar_width, 
+        edgecolor='white', label='Total Deposits')
+    
+    rects2 = plt.bar(r2, total_withdrawals, width=bar_width, 
+        edgecolor='white', label='Total Withdrawals')
+
+    plt.xlabel('Month')
+    plt.ylabel('Transactions Amount ($)')
+    plt.title('Expenses Report for %d: Transactions Grouped by Month' % user_year)
+    plt.xticks([r + bar_width for r in range(len(total_deposits))], 
+        list((digit_month_map).values()))
+
+    autolabel(rects1)
+    autolabel(rects2)
+
+    plt.legend()
+    plt.show()
